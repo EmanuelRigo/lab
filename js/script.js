d74a6e1ef305ffa50e93b8dbd64e467d43525bfb
EmanuelRigo <112441019+EmanuelRigo@users.noreply.github.com>
2023-07-31 18:53:20 -0300

    acces

---
+++ js/script.js
@@ -0,0 +1,56 @@
+const analisisContainer = document.getElementById("analisis-container")
+console.log(analisisContainer)
+
+function hola() {
+  alert("hola")
+}
+
+analisisContainer.addEventListener("click", hola)
+
+function cardsAnalisis(array, container) {
+  container.innerHTML = "";
+  for (item of array) {
+    let card = document.createElement("div");
+    card.className = "col d-flex justify-content-center";
+    card.id = item.id;
+    card.innerHTML = `
+    <div class="card tarjeta">
+      <div class="card-body">
+       <h5 class="card-title">${item.nombre} </h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Precio: $${item.precio}</li>
+      <li class="list-group-item">Demora: ${item.tiempo} dias </li>
+    </ul>
+    <div class="card-body">
+    <button type="button" class="btn btn-info">Agregar</button>
+    <button type="button" class="btn btn-warning">Quitar</button>
+    </div>
+  </div>`
+    container.append(card);
+  }
+
+}
+
+
+
+const urlArchivoJSON = new Request("analisis.json");
+let analisis = []
+fetch(urlArchivoJSON)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error al conseguir los datos.');
+    }
+    return response.json();
+  })
+  .then(data => {
+    analisis = analisis.concat(data);
+    cardsAnalisis(analisis, analisisContainer)
+    console.log(analisis)
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+
+console.log(analisis)
